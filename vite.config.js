4dfcc29311265a6bf958c79277539dca9ee9749b
malvaceae <malvaceae@hotmail.com>
2022-08-15 01:00:00 +0900

    add vite-plugin-html

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 // Vite
 import { defineConfig } from 'vite';
 
+// HTML
+import { createHtmlPlugin } from 'vite-plugin-html';
+
 export default defineConfig({
-  //
+  plugins: [
+    createHtmlPlugin({
+      entry: '/src/main.js',
+      inject: {
+        data: {
+          title: 'ChipTube',
+        },
+      },
+    }),
+  ],
 });
