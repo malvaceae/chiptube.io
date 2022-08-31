42aba6910d73ed7e37fcf9bf8b9fd68284d03c89
malvaceae <malvaceae@hotmail.com>
2022-09-01 00:00:00 +0900

    add autoprefixer and postcss

--- src/App.vue
+++ src/App.vue
@@ -97,7 +97,6 @@ const search = ref('');
 .mdi-google {
   color: transparent;
   background-image: conic-gradient(from 55deg at 40%, #4285f4 70deg, #0f9d58 70deg 175deg, #f4b400 175deg 255deg, #db4437 255deg 360deg);
-  -webkit-background-clip: text;
-          background-clip: text;
+  background-clip: text;
 }
 </style>
