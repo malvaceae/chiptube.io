0e7bd4a5d61e4df677fd433c5eebf732ee699956
malvaceae <malvaceae@hotmail.com>
2022-12-04 00:00:00 +0900

    update percussion instruments

--- src/components/TunePlayer.vue
+++ src/components/TunePlayer.vue
@@ -138,7 +138,7 @@ const ppq = computed(() => midi.value?.header?.ppq ?? 480);
 const tracks = computed(() => midi.value?.tracks ?? []);
 
 // instruments
-const instruments = shallowRef<(Tone.NoiseSynth | Tone.PolySynth | Tone.Sampler)[]>([]);
+const instruments = shallowRef<(Tone.PolySynth | Tone.Sampler)[]>([]);
 
 // current time in seconds
 const currentTime = ref(0);
@@ -279,7 +279,63 @@ const play = async () => {
 
   // samplers
   const samplers = tracks.value.reduce((samplers, { instrument: { number, percussion } }) => {
-    if (samplers[number] || percussion) {
+    if (samplers[number] && !percussion || percussion && samplers[128]) {
+      return samplers;
+    }
+
+    // percussion instruments
+    if (percussion) {
+      samplers[128] = new Tone.Sampler({
+        urls: {
+          'D#1': 'Ds1.mp3',
+          'E1': 'E1.mp3',
+          'F1': 'F1.mp3',
+          'F#1': 'Fs1.mp3',
+          'G1': 'G1.mp3',
+          'A#1': 'As1.mp3',
+          'B1': 'B1.mp3',
+          'C2': 'C2.mp3',
+          'C#2': 'Cs2.mp3',
+          'D2': 'D2.mp3',
+          'D#2': 'Ds2.mp3',
+          'E2': 'E2.mp3',
+          'F2': 'F2.mp3',
+          'F#2': 'Fs2.mp3',
+          'G2': 'G2.mp3',
+          'G#2': 'Gs2.mp3',
+          'A2': 'A2.mp3',
+          'A#2': 'As2.mp3',
+          'B2': 'B2.mp3',
+          'C3': 'C3.mp3',
+          'C#3': 'Cs3.mp3',
+          'D3': 'D3.mp3',
+          'D#3': 'Ds3.mp3',
+          'E3': 'E3.mp3',
+          'F3': 'F3.mp3',
+          'F#3': 'Fs3.mp3',
+          'G3': 'G3.mp3',
+          'G#3': 'Gs3.mp3',
+          'A3': 'A3.mp3',
+          'A#3': 'As3.mp3',
+          'B3': 'B3.mp3',
+          'C4': 'C4.mp3',
+          'C#4': 'Cs4.mp3',
+          'D#4': 'Ds4.mp3',
+          'E4': 'E4.mp3',
+          'F4': 'F4.mp3',
+          'F#4': 'Fs4.mp3',
+          'G4': 'G4.mp3',
+          'G#4': 'Gs4.mp3',
+          'A4': 'A4.mp3',
+          'A#4': 'As4.mp3',
+          'B4': 'B4.mp3',
+          'C5': 'C5.mp3',
+          'D5': 'D5.mp3',
+          'D#5': 'Ds5.mp3',
+        },
+        baseUrl: '/samples/percussion/',
+        release: 2,
+      }).toDestination();
       return samplers;
     }
 
@@ -287,24 +343,25 @@ const play = async () => {
     if (number === 0) {
       samplers[number] = new Tone.Sampler({
         urls: {
-          'A7': 'A7.mp3',
-          'A6': 'A6.mp3',
-          'A#5': 'As5.mp3',
-          'B4': 'B4.mp3',
-          'C3': 'C3.mp3',
-          'C#2': 'Cs2.mp3',
           'D1': 'D1.mp3',
-          'D#7': 'Ds7.mp3',
-          'D#6': 'Ds6.mp3',
-          'E5': 'E5.mp3',
-          'F4': 'F4.mp3',
-          'F#3': 'Fs3.mp3',
-          'G2': 'G2.mp3',
           'G#1': 'Gs1.mp3',
+          'C#2': 'Cs2.mp3',
+          'G2': 'G2.mp3',
+          'C3': 'C3.mp3',
+          'F#3': 'Fs3.mp3',
+          'F4': 'F4.mp3',
+          'B4': 'B4.mp3',
+          'E5': 'E5.mp3',
+          'A#5': 'As5.mp3',
+          'D#6': 'Ds6.mp3',
+          'A6': 'A6.mp3',
+          'D#7': 'Ds7.mp3',
+          'A7': 'A7.mp3',
         },
         baseUrl: '/samples/piano/',
         release: 2,
       }).toDestination();
+      return samplers;
     }
 
     return samplers;
@@ -312,23 +369,8 @@ const play = async () => {
 
   // instruments
   instruments.value = tracks.value.map(({ instrument: { number, percussion } }) => {
-    if (samplers[number] && !percussion) {
-      return samplers[number];
-    }
-
-    // percussion instruments
-    if (percussion) {
-      return new Tone.NoiseSynth({
-        noise: {
-          type: 'white',
-        },
-        envelope: {
-          attack: .001,
-          decay: .2,
-          sustain: .1,
-          release: .03,
-        },
-      }).toDestination();
+    if (samplers[number] && !percussion || percussion && samplers[128]) {
+      return samplers[!percussion ? number : 128];
     }
 
     // square synths (guitars)
@@ -449,12 +491,7 @@ const play = async () => {
     })(note.duration)));
 
     const part = new Tone.Part((time, { name, duration, velocity }) => {
-      // attack and release the note
-      if (instrument instanceof Tone.PolySynth || instrument instanceof Tone.Sampler) {
-        instrument.triggerAttackRelease(name, duration, time, velocity);
-      } else {
-        instrument.triggerAttackRelease(duration, time, velocity);
-      }
+      instrument.triggerAttackRelease(name, duration, time, velocity);
     }, notes);
 
     part.start();
@@ -485,6 +522,11 @@ const pause = () => {
   // pause
   Tone.Transport.pause();
 
+  // release all instruments
+  instruments.value.forEach((instrument) => {
+    instrument.releaseAll();
+  });
+
   // set current state to paused
   currentState.value = 'paused';
 };
@@ -496,6 +538,11 @@ const stop = () => {
 
   // cancel
   Tone.Transport.cancel();
+
+  // release all instruments
+  instruments.value.forEach((instrument) => {
+    instrument.releaseAll();
+  });
 
   // set current state to stopped
   currentState.value = 'stopped';
@@ -519,6 +566,11 @@ const toggle = () => {
 // seek
 const seek = (seconds: number) => {
   Tone.Transport.seconds = seconds;
+
+  // release all instruments
+  instruments.value.forEach((instrument) => {
+    instrument.releaseAll();
+  });
 };
 
 // seekbar value
