50eca15bc2caf256a21b316dfb0ec481a5291e42
malvaceae <malvaceae@hotmail.com>
2022-12-07 03:00:00 +0900

    add acoustic guitar samples

--- src/composables/instrument.ts
+++ src/composables/instrument.ts
@@ -4,7 +4,9 @@ import * as Tone from 'tone';
 // Sampler Name
 export type SamplerName =
   | 'bass-electric'
+  | 'guitar-acoustic'
   | 'guitar-electric'
+  | 'guitar-nylon'
   | 'percussion'
   | 'piano';
 
@@ -29,9 +31,14 @@ export const useInstrument = () => {
       return getSampler('piano');
     }
 
-    // square synths (guitars)
-    if (number >= 24 && number <= 25) {
-      return getSynth('guitar');
+    // acoustic guitar (nylon)
+    if (number === 24) {
+      return getSampler('guitar-nylon');
+    }
+
+    // acoustic guitar (steel)
+    if (number === 25) {
+      return getSampler('guitar-acoustic');
     }
 
     // electric guitar
@@ -101,6 +108,24 @@ export const useInstrument = () => {
             baseUrl: `/samples/${name}/`,
             release: 2,
           });
+        case 'guitar-acoustic':
+          // acoustic guitar (steel)
+          return new Tone.Sampler({
+            urls: {
+              'E2': 'E2.mp3',
+              'G2': 'G2.mp3',
+              'B2': 'B2.mp3',
+              'F3': 'F3.mp3',
+              'G#3': 'Gs3.mp3',
+              'C4': 'C4.mp3',
+              'F4': 'F4.mp3',
+              'A4': 'A4.mp3',
+              'C#5': 'Cs5.mp3',
+              'D5': 'D5.mp3',
+            },
+            baseUrl: `/samples/${name}/`,
+            release: 2,
+          });
         case 'guitar-electric':
           // electric guitar
           return new Tone.Sampler({
@@ -114,6 +139,29 @@ export const useInstrument = () => {
               'A4': 'A4.mp3',
               'C5': 'C5.mp3',
               'D#5': 'Ds5.mp3',
+            },
+            baseUrl: `/samples/${name}/`,
+            release: 2,
+          });
+        case 'guitar-nylon':
+          // acoustic guitar (nylon)
+          return new Tone.Sampler({
+            urls: {
+              'B1': 'B1.mp3',
+              'F#2': 'Fs2.mp3',
+              'G#2': 'Gs2.mp3',
+              'C#3': 'Cs3.mp3',
+              'D3': 'D3.mp3',
+              'E3': 'E3.mp3',
+              'G3': 'G3.mp3',
+              'A3': 'A3.mp3',
+              'B3': 'B3.mp3',
+              'D#4': 'Ds4.mp3',
+              'F#4': 'Fs4.mp3',
+              'C#5': 'Cs5.mp3',
+              'E5': 'E5.mp3',
+              'G#5': 'Gs5.mp3',
+              'A5': 'A5.mp3',
             },
             baseUrl: `/samples/${name}/`,
             release: 2,
