34df20e1d23f27bb9726c3004e55a0702ea1493c
malvaceae <malvaceae@hotmail.com>
2022-12-10 13:00:00 +0900

    add pizzicato samples

--- src/composables/instrument.ts
+++ src/composables/instrument.ts
@@ -17,6 +17,7 @@ export type SamplerName =
   | 'percussion'
   | 'piano'
   | 'piccolo'
+  | 'pizzicato'
   | 'seashore'
   | 'strings'
   | 'timpani'
@@ -80,8 +81,13 @@ export const useInstrument = () => {
     }
 
     // sawtooth synths (stringed instruments)
-    if (number >= 40 && number <= 45) {
+    if (number >= 40 && number <= 44) {
       return getSynth('stringed');
+    }
+
+    // pizzicato
+    if (number === 45) {
+      return getSampler('pizzicato');
     }
 
     // harp
@@ -503,6 +509,26 @@ export const useInstrument = () => {
               'E6': 'E6.mp3',
               'A6': 'A6.mp3',
               'C7': 'C7.mp3',
+            },
+            baseUrl: `/samples/${name}/`,
+            release: 2,
+          });
+        case 'pizzicato':
+          // pizzicato
+          return new Tone.Sampler({
+            urls: {
+              'G1': 'G1.mp3',
+              'B1': 'B1.mp3',
+              'F2': 'F2.mp3',
+              'C3': 'C3.mp3',
+              'E3': 'E3.mp3',
+              'D4': 'D4.mp3',
+              'A4': 'A4.mp3',
+              'C5': 'C5.mp3',
+              'G5': 'G5.mp3',
+              'B5': 'B5.mp3',
+              'F6': 'F6.mp3',
+              'D7': 'D7.mp3',
             },
             baseUrl: `/samples/${name}/`,
             release: 2,
